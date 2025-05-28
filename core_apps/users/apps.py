93cd192d7f1e2db6e90cbf920acfa0225caf2072
vitorguima <vitorguima@gmail.com>
2025-05-28 18:11:24 -0300

    build: ignore elasticsearch for now

--- core_apps/users/apps.py
+++ core_apps/users/apps.py
@@ -5,6 +5,3 @@ from django.utils.translation import gettext_lazy as _
 class UsersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "core_apps.users"
-
-
-# PROJECT SETUP S2 - class 8
